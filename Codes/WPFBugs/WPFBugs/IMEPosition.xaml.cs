968d88f7754ba78efcf614883c35a3b316e2cfcf
Iron <iron.yehong@outlook.com>
2018-09-24 16:56:53 +0800

    Initialize project with TextBox problem

---
+++ Codes/WPFBugs/WPFBugs/IMEPosition.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace WPFBugs
+{
+    /// <summary>
+    /// Interaction logic for IMEPosition.xaml
+    /// </summary>
+    public partial class IMEPosition
+    {
+        [DllImport("User32.dll")]
+        private static extern IntPtr SetFocus(IntPtr hWnd);
+
+        public IMEPosition()
+        {
+            InitializeComponent();
+            MyPopup.GotFocus += MyPopup_GotFocus;
+        }
+
+        private void MyPopup_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var source = (HwndSource)PresentationSource.FromVisual(MyPopup.Child);
+            if (source != null)
+            {
+                SetFocus(source.Handle);
+                MyTextBox.Focus();
+            }
+        }
+    }
+}
---
+++ Codes/WPFBugs/WPFBugs/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace WPFBugs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void IMEPositionButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            new IMEPosition().ShowDialog();
+        }
+    }
+}
